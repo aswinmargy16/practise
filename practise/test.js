e7c4544abefceac582746c0ce83968faa0f60e3c
aswinmargy16 <aswinmargy16@gmail.com>
2022-09-28 11:52:41 +0530

    ok

---
+++ practise/test.js
@@ -0,0 +1,17 @@
+var a=10;
+var b=20;
+var c="thirty";
+var d="red"
+var e=50.5;
+var f=true;
+var g="venkey";
+var h="thrisha";
+var i="false";
+console.log(a+b);
+console.log(a+c);
+console.log(a+d);
+console.log(a+e);
+console.log(a+f);
+console.log(a+g);
+console.log(a+i);
+console.log(a+h);
