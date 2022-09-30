6c20f2ede0c40aed943e714bc8d4c022febb903d
aswinmargy16 <aswinmargy16@gmail.com>
2022-09-30 11:33:19 +0530

    hi

---
+++ assignment/divisible.js
@@ -0,0 +1,5 @@
+if (Number %8 === 2) {
+    console. log("divisible")
+    } else { 
+    console. log("not divisible")
+    }
---
+++ assignment/three.js
@@ -0,0 +1,6 @@
+if (Number %2 == 1) {
+    console. log("multiple")
+    } 
+    else { 
+    console. log("not multiple")
+    }
