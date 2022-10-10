b6c598471ae62777ee2cc1901bc2fb738d37bdac
aswinmargy16 <aswinmargy16@gmail.com>
2022-10-10 13:13:28 +0530

    okk

--- assignment/array/arraymethods/one.js
+++ assignment/array/arraymethods/one.js
@@ -1,4 +1,11 @@
-let employee=[{id:101,name:"venkey",salary:4000,
-id:102,name:"thrisha",salary:4500}]
-function createemployee(employee){
-    employee.push(employee)}
+let employee=[{id:101,name:"venkey",salary:4000},
+{id:102,name:"thrisha",salary:4500}]
+function createemployee(emp){
+    employee.push(emp)
+}
+createemployee({id:103,name:"shakthi",salary:30000})
+console.log(employee)
+createemployee({id:104,name:"hanumanthu",salary:35000})
+console.log(employee)
+createemployee({id:105,name:"meenakshi",salary:45000})
+console.log(employee)
